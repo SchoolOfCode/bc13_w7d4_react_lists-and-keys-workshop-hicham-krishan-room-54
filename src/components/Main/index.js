360443319dcb8a4c948e256346398f3d72aeaaf2
HBE17 <hicham-boutaghou@hotmail.co.uk>
2022-11-11 09:18:14 +0000

    latest version

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './index.css';
+import "./index.css";
 
-import samplePosts from '../../libs/data';
+import samplePosts from "../../libs/data";
 
-import Post from '../Post';
+import Post from "../Post";
 
+//use .map() to loop over the posts state
+//for each post in the array return a <post></post> component
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  return (
+    <main id="main">
+      {posts.map((post) => {
+        return <Post key={post.postId}></Post>;
+      })}
+    </main>
+  );
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-function Post() {
-  return <article></article>;
+function Post({ title, date, author, text, highlights, image }) {
+  return (
+    <div>
+      <p>{title}</p>
+      <p>{date}</p>
+      <p>{author}</p>
+      <article>{text}</article>
+      <summary>{highlights}</summary>
+    </div>
+  );
 }
 
 export default Post;
